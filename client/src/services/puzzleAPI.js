2288260af3799eb88c79daf6e6a55056b9ce53ec
Francois Le Roux <francois.leroux@sirris.be>
2025-01-14 10:16:56 +0100

    update data

--- client/src/services/puzzleAPI.js
+++ client/src/services/puzzleAPI.js
@@ -1,7 +1,8 @@
 // client/src/services/puzzleAPI.js
 export async function fetchPuzzleOfTheDay() {
     // const response = await fetch('http://localhost:4000/api/puzzles/today');
-    const response = await fetch('http://81.164.23.45:4000/api/puzzles/today');
+    // const response = await fetch('http://81.164.23.45:4000/api/puzzles/today');
+    const response = await fetch('https://french-connections-production.up.railway.app:8080/api/puzzles/today');
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
